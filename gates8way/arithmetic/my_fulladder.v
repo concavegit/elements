2a932d6d28d4323bdbd9dbaba4c173a9e91602c8
Kawin Nikomborirak <concavemail@gmail.com>
2018-08-15 23:49:46 +0000

    Cleanup adder16

--- gates8way/arithmetic/my_fulladder.v
+++ gates8way/arithmetic/my_fulladder.v
@@ -1,13 +1,14 @@
 `include "my_halfadder"
+
 module my_fulladder (input a,
                      input  b,
                      input  c,
                      output sum,
                      output carry);
 
-   wire                     sum1, carry1, carry2;
-   
-   my_halfadder h1(a, b, sum1, carry1);
-   my_halfadder h2(sum1, c, sum, carry2);
-   or (carry, carry1, carry2);
+   wire                     sum0, carry0, carry1;
+
+   my_halfadder h0(a, b, sum0, carry0),
+     h1(sum0, c, sum, carry1);
+   or (carry, carry0, carry1);
 endmodule
