a09047af5991b8b22043d96aae98ae48c24d1aa7
Kawin Nikomborirak <concavemail@gmail.com>
2018-08-15 23:51:45 +0000

    squash! Cleanup fulladder

--- gates8way/arithmetic/my_add16.v
+++ gates8way/arithmetic/my_add16.v
@@ -1,10 +1,10 @@
 `include "my_fulladder.v"
 
-module my_add16 (input [15:0] a,
-                 input [15:0]  b,
+module my_add16 (input [15:0] a, b,
                  output [15:0] out);
 
-   wire                        carry1,
+   wire                        carry0,
+                               carry1,
                                carry2,
                                carry3,
                                carry4,
@@ -17,24 +17,26 @@ module my_add16 (input [15:0] a,
                                carry11,
                                carry12,
                                carry13,
-                               carry14,
-                               carry15;
+                               carry14;
 
-   my_halfadder h1(a[15], b[15], out[15], carry1);
-   my_fulladder h2(a[14], b[14], carry1, out[14], carry2);
-   my_fulladder h3(a[13], b[13], carry2, out[13], carry3);
-   my_fulladder h4(a[12], b[12], carry3, out[12], carry4);
-   my_fulladder h5(a[11], b[11], carry4, out[11], carry5);
-   my_fulladder h6(a[10], b[10], carry5, out[10], carry6);
-   my_fulladder h7(a[9], b[9], carry6, out[9], carry7);
-   my_fulladder h8(a[8], b[8], carry7, out[8], carry8);
-   my_fulladder h9(a[7], b[7], carry8, out[7], carry9);
-   my_fulladder h10(a[6], b[6], carry9, out[6], carry10);
-   my_fulladder h111(a[5], b[5], carry10, out[5], carry11);
-   my_fulladder h112(a[4], b[4], carry11, out[4], carry12);
-   my_fulladder h113(a[3], b[3], carry12, out[3], carry13);
-   my_fulladder h114(a[2], b[2], carry13, out[2], carry14);
-   my_fulladder h115(a[1], b[1], carry14, out[1], carry15);
-   xor(out[0], a[0], b[0], carry15);
+
+   my_halfadder h0(a[15], b[15], out[15], carry0);
+
+   my_fulladder h1(a[14], b[14], carry0, out[14], carry1),
+     h2(a[13], b[13], carry1, out[13], carry2),
+     h3(a[12], b[12], carry2, out[12], carry3),
+     h4(a[11], b[11], carry3, out[11], carry4),
+     h5(a[10], b[10], carry4, out[10], carry5),
+     h6(a[9], b[9], carry5, out[9], carry6),
+     h7(a[8], b[8], carry6, out[8], carry7),
+     h8(a[7], b[7], carry7, out[7], carry8),
+     h9(a[6], b[6], carry8, out[6], carry9),
+     h10(a[5], b[5], carry9, out[5], carry10),
+     h11(a[4], b[4], carry10, out[4], carry11),
+     h12(a[3], b[3], carry11, out[3], carry12),
+     h13(a[2], b[2], carry12, out[2], carry13),
+     h14(a[1], b[1], carry13, out[1], carry14);
+
+   xor(out[0], a[0], b[0], carry14);
 
 endmodule
--- gates8way/testbench.cpp
+++ gates8way/testbench.cpp
@@ -1,5 +1,6 @@
 #include <verilated.h>
 #include <verilated_vcd_c.h>
+#include <cmath>
 
 #include <Vmy_gates8way.h>
 
@@ -17,9 +18,24 @@ int main(int argc, char** argv)
   top.trace(&tfp, 99);
   tfp.open("obj_dir/trace.vcd");
 
-  for (auto i = 0; i < 256; i++)
+  for (auto i = 0; i < pow(2, 131); i++)
     {
-      top.in = i;
+      top.sel = i % 8;
+      int ins[8];
+      for (auto j = 0; j < 8; j++)
+        {
+          ins[j] = (i % int(pow(2, 16 * j + 19))) / pow(2, 16 * j + 3);
+        }
+
+      top.in0 = ins[0];
+      top.in1 = ins[1];
+      top.in2 = ins[2];
+      top.in3 = ins[3];
+      top.in4 = ins[4];
+      top.in5 = ins[5];
+      top.in6 = ins[6];
+      top.in7 = ins[7];
+
       top.eval();
       tfp.dump(main_time);
       main_time++;
