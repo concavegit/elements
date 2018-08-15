40576e146c88d5af98abc83372603b435587a469
Kawin Nikomborirak <concavemail@gmail.com>
2018-08-15 16:31:09 +0000

    Reduce size of mux

--- gates/my_mux.v
+++ gates/my_mux.v
@@ -3,16 +3,10 @@ module my_mux(input in1,
               input  sel,
               output out);
 
-   wire              a, b, c, d, e, f, g;
+   wire              a, b, c;
 
    nand (a, sel, sel),
      (b, in1, a),
-     (c, b, b),
-
-     (d, sel, in2),
-     (e, d, d),
-
-     (f, c, c),
-     (g, e, e),
-     (out, f, g);
+     (c, sel, in2),
+     (out, b, c);
 endmodule
