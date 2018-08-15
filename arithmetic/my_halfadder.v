c7614dcb07994c06640761bdaa9d8b5550d4ae91
Kawin Nikomborirak <concavemail@gmail.com>
2018-08-15 23:56:46 +0000

    Move arithmetic
    This reverts commit 97d0e4f1ba88f974a3deb86d0d55fdc16cb94347.

--- arithmetic/my_halfadder.v
+++ arithmetic/my_halfadder.v
@@ -2,11 +2,11 @@ module my_halfadder (input a, b,
                      output sum, carry);
 
    wire                     c, d, e;
-
+   
    nand (c, a, b),
      (carry, c, c),
 
      (d, c, a),
      (e, c, b),
      (sum, d, e);
-endmodule
+endmodule 
