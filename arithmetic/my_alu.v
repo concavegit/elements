7325156f4a3c162fdee56198ccd68a049c4a9ffc
Kawin Nikomborirak <concavemail@gmail.com>
2018-08-16 01:23:37 +0000

    Implement alu

--- arithmetic/my_alu.v
+++ arithmetic/my_alu.v
@@ -1,3 +1,7 @@
+`include "my_add16.v"
+`include "../gates8way/my_mux16.v"
+`include "my_or16way.v"
+
 module my_alu (input [15:0] x,
                input [15:0]  y,
                input         zx,
@@ -10,5 +14,36 @@ module my_alu (input [15:0] x,
                output        zr,
                output        ng);
 
-   wire                      x1, x2;
-endmodule 
+   wire [15:0]               zx0, nx0, zy0, ny0, f0, f1, f2;
+   wire                      nzx, nzy;
+
+   not (nzx, zx),
+     (nzy, zy);
+
+   genvar                    i;
+
+   generate
+      for (i = 0; i < 16; i++) begin
+         and (zx0[i], x[i], nzx),
+        (zy0[i], y[i], nzy);
+      end
+
+      for (i = 0; i < 16; i++) begin
+         xor (nx0[i], zx0[i], nx),
+        (ny0[i], zy0[i], ny);
+      end
+   endgenerate
+
+   and (f0, nx0, ny0);
+   my_add16 aw(nx0, ny0, f1);
+   my_mux16 mw(f0, f1, f, f2);
+
+   generate
+      for (i = 0; i < 16; i++) begin
+         xor (out[i], f2[i], no);
+      end
+   endgenerate
+
+   my_or16way ow(out, zr);
+   assign ng = out[0];
+endmodule
