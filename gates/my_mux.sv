a484d24e949ced4557e1a8b01e78a837dbcb8e17
Kawin Nikomborirak <concavemail@gmail.com>
2018-08-19 18:42:48 +0000

    Use in0 and in1 instead of in1 and in2

--- gates/my_mux.sv
+++ gates/my_mux.sv
@@ -1,10 +1,10 @@
-module my_mux (input in1, in2, sel,
+module my_mux (input in0, in1, sel,
                output out);
 
    bit                a, b, c;
 
    nand (a, sel, sel),
-     (b, in1, a),
-     (c, sel, in2),
+     (b, in0, a),
+     (c, sel, in1),
      (out, b, c);
 endmodule
--- gates8way/my_mux16.sv
+++ gates8way/my_mux16.sv
@@ -1,4 +1,4 @@
-module my_mux16 (input shortint in1, in2,
+module my_mux16 (input shortint in0, in1,
                  bit             sel,
                  output shortint out);
 
@@ -10,9 +10,9 @@ module my_mux16 (input shortint in1, in2,
    genvar                        i;
 
    generate
-      for (i = 0; i < $bits(in1); i = i + 1) begin
-         nand (aa[i], in1[i], a),
-        (bb[i], in2[i], sel);
+      for (i = 0; i < $bits(in0); i = i + 1) begin
+         nand (aa[i], in0[i], a),
+        (bb[i], in1[i], sel);
       end
    endgenerate
 
