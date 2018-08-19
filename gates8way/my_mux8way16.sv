ec8c776ceb9d74e500bb709f086addd0cedf1cad
Kawin Nikomborirak <concavemail@gmail.com>
2018-08-19 18:43:13 +0000

    Fix mux8way16

--- gates8way/my_mux8way16.sv
+++ gates8way/my_mux8way16.sv
@@ -11,13 +11,13 @@ module my_mux8way16 (input shortint in0,
 
    shortint                          a, b, c, d, e, f;
 
-   my_mux16 m0 (in0, in4, sel[0], a),
-     m1 (in1, in5, sel[0], b),
-     m2 (in2, in6, sel[0], c),
-     m3 (in3, in7, sel[0], d),
+   my_mux16 m0 (in0, in1, sel[0], a),
+     m1 (in2, in3, sel[0], b),
+     m2 (in4, in5, sel[0], c),
+     m3 (in6, in7, sel[0], d),
 
-     m4 (a, c, sel[1], e),
-     m5 (b, d, sel[1], f),
+     m4 (a, b, sel[1], e),
+     m5 (c, d, sel[1], f),
 
      m6 (e, f, sel[2], out);
 endmodule
